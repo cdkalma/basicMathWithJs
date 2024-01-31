48aeb80975b3ddfdf2ff6c42da02f67259f0b26a
crdCalma <cdguttierrez11@gmail.com>
2024-01-31 04:34:08 -0500

    cosas

--- analisis.js
+++ analisis.js
@@ -41,3 +41,19 @@ para finalmente determinar cuál sería el nuevo salario */
   
     return nuevoSalario;
 }
+
+//análisis empresarial
+const empresas = {}
+for (persona of salarios) {
+    for (trabajo of persona.trabajos){
+        if (!empresas[trabajo.empresa]) {
+            empresas[trabajo.empresa] = {}
+        }
+        if (!empresas[trabajo.empresa][trabajo.year]) {
+            empresas[trabajo.empresa][trabajo.year] = []
+        }
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario)
+    }
+}
+
+console.log(empresas)
